080c910cd07d859aac4bfa6e4dacbf4dd15a2c29
Eloi Sanchez <eloisanchez16@gmail.com>
2025-03-05 10:57:31 +0100

    added level selection

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -1,14 +1,31 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MarginContainer/VBoxContainer/CenterContainer/MainButtons
+@onready var level_selection: GridContainer = $MarginContainer/VBoxContainer/CenterContainer/LevelMenu/LevelSelection
+@onready var level_menu: VBoxContainer = $MarginContainer/VBoxContainer/CenterContainer/LevelMenu
 
+var game = preload("res://scenes/game.tscn")
+
+func _ready():
+	for button in level_selection.get_children():
+		button.pressed.connect(_to_level.bind(button.text))
 
 func _on_start_button_button_up() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
+	get_tree().change_scene_to_packed(game)
 
 func _on_select_level_button_button_up() -> void:
-	pass # Replace with function body.
-
+	main_buttons.hide()
+	level_menu.show()
 
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
+
+func _to_level(level: String):
+	var game_instance = game.instantiate()
+	game_instance.level_number = int(level)
+	get_tree().root.add_child(game_instance)
+	queue_free()
+
+func _on_back_button_pressed() -> void:
+	main_buttons.show()
+	level_menu.hide()
